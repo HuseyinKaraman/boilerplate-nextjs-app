e6692656ba4049e83a8a9423789965b9ac03ee09
Hercules <hsy.krmn@gmail.com>
2024-01-16 00:53:15 +0300

    edit env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         MONGODB_URI:process.env.MONGODB_URI,
-        NEXT_PUBLIC_API_URL:process.env.NODE_ENV === 'production' ? 'https://nextecom-server.vercel.app' : process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_API_URL:process.env.NODE_ENV === 'production' ? 'https://nextecom-dusky.vercel.app/api' : process.env.NEXT_PUBLIC_API_URL,
         NEXTAUTH_SECRET:process.env.NEXTAUTH_SECRET,
         GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET
